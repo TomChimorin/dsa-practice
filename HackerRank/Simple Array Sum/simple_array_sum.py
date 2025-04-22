dd4f242bc429808425e3fec934913f557137b553
Yu Zhao <zhaoyu0528@gmail.com>
2025-04-22 02:31:15 -0400

    HackerRank Solve Me First and Simple Array Sum

---
+++ HackerRank/Simple Array Sum/simple_array_sum.py
@@ -0,0 +1,6 @@
+def simpleArraySum(ar):
+    # Write your code here
+    total = 0
+    for num in ar:
+        total += num
+    return total
---
+++ HackerRank/Solve Me First/solve_me_first.py
@@ -0,0 +1,9 @@
+
+num1 = int(input())
+num2 = int(input())
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    
+    print(a+b)
+
+solveMeFirst(num1, num2)
